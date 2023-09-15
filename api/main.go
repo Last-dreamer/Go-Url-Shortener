af73f0a4b4f28b466cf4c78acdb8eb3d13d656cb
Last-dreamer <asim0000.ak@gmail.com>
2023-09-15 22:48:49 +0400

    setting up main with routes with fiber

--- api/main.go
+++ api/main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"os"
+	"url-shortner/routes"
+
+	"github.com/gofiber/fiber"
+	"github.com/subosito/gotenv"
+	"gorm.io/gorm/logger"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := gotenv.Load()
+
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	app := fiber.New()
+	app.Use(logger.New())
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,1 +1,17 @@
 package routes
+
+import "time"
+
+type Request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type Response struct {
+	URL            string        `json:"url"`
+	CustomShort    string        `json:"short"`
+	Expiry         time.Duration `json:"expiry"`
+	XRateRemaining int           `json:"rate_limit"`
+	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
