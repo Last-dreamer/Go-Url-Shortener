f535bd038d6a54fb59df20499e5aadd00b68687d
Last-dreamer <asim0000.ak@gmail.com>
2023-09-16 23:58:55 +0400

    added shorten url error functionaly to main

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,11 @@
 package routes
 
-import "time"
+import (
+	"time"
+	"url-shortner/helpers"
+
+	"github.com/gofiber/fiber"
+)
 
 type Request struct {
 	URL         string        `json:"url"`
@@ -14,4 +19,27 @@ type Response struct {
 	Expiry         time.Duration `json:"expiry"`
 	XRateRemaining int           `json:"rate_limit"`
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(Request)
+
+	if err := c.BodyParser(&body); err != nil {
+
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse json"})
+	}
+
+	if !goValidator.IsUrl(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	// enforce https etc
+	body.URL = helpers.EnforceHTTP(body.URL)
+
+	return nil
 }
