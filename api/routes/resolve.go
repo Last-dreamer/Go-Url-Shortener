7da7575668d4b28e15117dc35f0d61155a0319e9
Last-dreamer <asim0000.ak@gmail.com>
2023-09-21 20:48:30 +0400

    adding latest  code

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"url-shortner/database"
+
+	"github.com/gofiber/fiber"
+)
+
+func ResolveURL(c *fiber.Ctx) {
+
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	r.Get(url)
+}
