516d110f7ceb7266627469e5384ea4703215226a
Last-dreamer <asim0000.ak@gmail.com>
2024-04-21 23:24:14 +0400

    added limiter

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
+	"url-shortner/database"
 	"url-shortner/helpers"
 
+	"github.com/go-redis/redis"
 	"github.com/gofiber/fiber"
 )
 
@@ -25,8 +29,26 @@ func ShortenURL(c *fiber.Ctx) error {
 	body := new(Request)
 
 	if err := c.BodyParser(&body); err != nil {
-
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse json"})
+	}
+
+	// rate limiter
+
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	val, err := r2.Get(c.IP()).Result()
+	if err == redis.Nil {
+		r2.Set(c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err() // for 30 min
+	} else {
+		value, _ := strconv.Atoi(val)
+		if value <= 0 {
+			limit, _ := r2.TTL(c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
 	}
 
 	if !goValidator.IsUrl(body.URL) {
@@ -40,6 +62,8 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// enforce https etc
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(c.IP())
 
 	return nil
 }
